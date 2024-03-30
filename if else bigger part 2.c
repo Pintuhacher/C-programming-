b21ef900c9c3d0b5c43986fccf5ddf36ffd07a99
Pintuhacher <164980337+Pintuhacher@users.noreply.github.com>
2024-03-30 18:50:27 +0530

    Update

---
+++ if else bigger part 2.c
@@ -0,0 +1,19 @@
+int  main() {
+    int a,b,c;
+    printf("Enter the value of a: ");
+    scanf("%d",&a);
+    printf( "Enter the value of b: ");
+    scanf("%d", &b);
+    printf("enter the vaiue of c");
+scanf( "%d", &c );
+if (a>b)
+   printf("a is bigger");
+   else
+   {
+    if (b>c)
+        printf("b is bigger");
+        else
+        printf("c is bigger");
+            }
+    
+   }
