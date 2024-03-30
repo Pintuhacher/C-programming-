8888e06be18d93a1f2d72fce6555257641d83f07
Pintuhacher <164980337+Pintuhacher@users.noreply.github.com>
2024-03-30 18:48:03 +0530

    Update

---
+++ temperature conversion.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int  main() {
+    float  a, b;
+    printf("Enter two numbers: ");
+    scanf("%f", &a);
+    b=(a*9/5)+32;
+    printf("\n Fahrenheit is %f",b);
+    return 0;
+    }
